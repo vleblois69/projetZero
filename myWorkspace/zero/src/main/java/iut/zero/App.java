e9d21378d305a3f96b5f203bc9de1920e4548193
Vincent LEBLOIS <Vincent LEBLOIS>
2017-04-18 09:42:18 +0200

    Création du projet zero Java Maven

---
+++ myWorkspace/zero/src/main/java/iut/zero/App.java
@@ -0,0 +1,13 @@
+package iut.zero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
