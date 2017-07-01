11205bae67459e94cae9296ed4ea3dc2ca1ec668
Vincent LEBLOIS <Vincent LEBLOIS>
2017-07-01 21:00:03 +0200

    Ajout attribut couleur

--- src/main/java/iut/zero/Arme.java
+++ src/main/java/iut/zero/Arme.java
@@ -1,10 +1,13 @@
 package iut.zero;
 
+import org.eclipse.swt.graphics.Color;
+
 public class Arme {
 	
 	protected String nom;
 	protected String description;
 	protected int degats;
 	protected int prix;
+	protected Color couleur;
 
 }
