721f362f5af1e5e68512096f3211d6b711397bd1
Vincent LEBLOIS <Vincent LEBLOIS>
2017-07-02 12:47:07 +0200

    Ajout des getters et setters

--- src/main/java/iut/zero/Arme.java
+++ src/main/java/iut/zero/Arme.java
@@ -9,5 +9,37 @@ public class Arme {
 	protected int degats;
 	protected int prix;
 	protected Color couleur;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getDegats() {
+		return degats;
+	}
+	public void setDegats(int degats) {
+		this.degats = degats;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public Color getCouleur() {
+		return couleur;
+	}
+	public void setCouleur(Color couleur) {
+		this.couleur = couleur;
+	}
+	
+	
 
 }
